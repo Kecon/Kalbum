2f64da2c08e73ad79c0fd541a033105779a3b5c1
Kenny Colliander <kecon81@gmail.com>
2024-06-24 12:52:29 +0200

    Add resource configuration for no store

---
+++ src/main/java/se/kecon/kalbum/WebConfig.java
@@ -0,0 +1,34 @@
+package se.kecon.kalbum;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * WebConfig for configuring resources and headers
+ *
+ * @author Kenny Colliander
+ * @since 2024-06-24
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CacheControlInterceptor()).addPathPatterns("/index.html");
+        registry.addInterceptor(new CacheControlInterceptor()).addPathPatterns("/login");
+    }
+
+    private static class CacheControlInterceptor implements HandlerInterceptor {
+
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            if (request.getMethod().equals("GET")) {
+                response.setHeader("Cache-Control", "no-store");
+            }
+            return true;
+        }
+    }
+}
